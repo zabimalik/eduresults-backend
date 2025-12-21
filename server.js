ed1a49dea62d9d0245fbfbfb2920cef13669e3ee
zabimalik <zabimalik@example.com>
2025-12-20 21:08:13 -0800

    cv

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'development' 
-    ? ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:4173', 'http://127.0.0.1:5173', 'http://localhost:8080', 'http://127.0.0.1:8080']
+    ? ['http://localhost:5173', 'https://eduresults-hub-tawny.vercel.app','https://eduresults-hub-tawny.vercel.app/', 'http://localhost:3000', 'http://localhost:4173', 'http://127.0.0.1:5173', 'http://localhost:8080', 'http://127.0.0.1:8080']
     : process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
